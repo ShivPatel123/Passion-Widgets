68ce42d842f7d094a27a021b6ea893909a42f19e
Shivansh Patel <shivanshpatel123@gmail.com>
2022-08-11 01:48:44 -0500

    Initial Commit

---
+++ Widgets App/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Widgets App
+//
+//  Created by Shiv Patel on 8/11/22.
+//
+import WidgetKit
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to Widgets!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+  
---
+++ Widgets App/Widgets_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Widgets_AppApp.swift
+//  Widgets App
+//
+//  Created by Shiv Patel on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Widgets_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
